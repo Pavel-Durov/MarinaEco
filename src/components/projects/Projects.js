817e718f615ca15ad769a3307578374f096930d6
Pavel-Durov <pdurov0@gmail.com>
2018-04-21 18:41:32 +0100

    routing using navigation query params

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { selectProject } from '../../actions/ProjectActions';
 import { getTranslate } from 'react-localize-redux';
-import { not, isNil, isEmpty, clone } from 'ramda';
+import { not, isNil, isEmpty, clone, find, propEq } from 'ramda';
+import * as R from 'ramda';
 
 import ProjectDetails from './details/ProjectDetails';
 import MainMenu from '../common/mainMenu/MainMenu';
@@ -15,38 +16,47 @@ import './projects.css';
 class Projects extends React.Component {
     constructor(props, context) {
         super(props, context);
+        this.selectedProject = {};
         this.onProjectSelected = this.onProjectSelected.bind(this);
     }
 
     componentWillUnmount() {
         this.props.dispatch(selectProject(undefined));
     }
+
     onProjectSelected(index) {
         const project = this.props.projectsCollection[index];
         this.props.dispatch(selectProject(project));
     }
+
+    componentWillUpdate(props) {
+        const id = parseInt(props.location.query.id);
+        const project = R.find(R.propEq('id', id), this.props.projectsCollection);
+        this.selectedProject = project || {};
+    }
+
     render() {
         return (
             <div>
-                <MainMenu />
+                <MainMenu/>
                 <div id="outer-container">
                     {
-                        isNil(this.props.selectedProject.name) ?
+                        isNil(this.selectedProject.name) ?
                             (<div>
                                 {
                                     not(isEmpty(this.props.projectsCollection)) ?
                                         <Gallery enableImageSelection={false} onClickThumbnail={this.onProjectSelected}
                                             images={clone(this.props.projectsCollection)} />
-                                        : <div/>
+                                        : <div />
                                 }
                             </div>)
                             :
                             (<div>
                                 <h1 className="project-bread-crumbs">
                                     /{this.props.translate('projects')}
-                                    {this.props.selectedProject ? `/${this.props.selectedProject.name}` : ''}
+                                    {this.selectedProject ? `/${this.selectedProject.name}` : ''}
                                 </h1>
-                                <ProjectDetails project={this.props.selectedProject} />
+                                <ProjectDetails project={this.selectedProject} />
                             </div>)
                     }
                 </div>
@@ -57,7 +67,6 @@ class Projects extends React.Component {
 
 function mapStateToProps(state, ownProps) {
     return {
-        selectedProject: state.projects.selectedProject || {},
         projectsCollection: state.projects.projectsCollection || [],
         translate: getTranslate(state.localeReducer),
     };
